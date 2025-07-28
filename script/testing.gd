4d08b6b1b7db2393f13f24c4258d56fe3e039810
Boz <mondohamoeva@hotmail.fr>
2025-07-28 10:22:54 +0200

    adding ink

--- script/testing.gd
+++ script/testing.gd
@@ -1,18 +1,31 @@
 extends Node2D
 
-var random_data: String = "random_data"
-var player_data: Dictionary = {
-    "name": "Player",
-    "health": 100,
-    "instincts": {
-        "courage": 0,
-        "wisdom": 0,
-        "compassion": 0
-    }
-}
+
+var _ink_player: InkPlayer
 
 func _ready() -> void:
-    # Initialisation du jeu
-    print("Game is ready")
-    print("Random Data: ", random_data)
-    print("Player Data: ", player_data)
+	_initialize_ink_player()
+
+
+func _initialize_ink_player() -> void:
+	_ink_player = InkPlayer.new()
+	add_child(_ink_player)
+	_ink_player.ink_file = load("res://ink/jsons/testing_story.json")
+	_ink_player.loads_in_background = true
+	_ink_player.connect("loaded", Callable(self, "_on_story_loaded"))
+	_ink_player.create_story()
+
+func _on_story_loaded(success: bool) -> void:
+	print("Story loaded: ", success)
+	if success:
+		_continue_story()
+	else:
+		print("Failed to load the story")
+
+func _emit_story_loaded_signal(success: bool) -> void:
+	print("Emitting story loaded signal")
+	emit_signal("story_loaded", success)
+
+func _continue_story() -> void:
+	print("Continuing story")
+	print(_ink_player.continue_story())
